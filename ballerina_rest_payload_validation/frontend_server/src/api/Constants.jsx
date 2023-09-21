57880f3601a3cd8088797bb304eec72e51dc55fb
Sasindu Alahakoon <dilsharasasindu@gmail.com>
2023-09-21 10:49:59 +0530

    Add basic URL for payload validation

--- ballerina_rest_payload_validation/frontend_server/src/api/Constants.jsx
+++
@@ -1,7 +0,0 @@
-export const serverUrl = "http://localhost:9090";
-
-// POST requests
-export const submitOrderUrl = serverUrl + "/sales/orders";
-
-// GET requests
-export const getOrderUrl = serverUrl + "/sales/orders";
---
+++ ballerina_rest_payload_validation/frontend_server/src/sections/order/orders-table.js
@@ -0,0 +1,95 @@
+import PropTypes from 'prop-types';
+import {
+  Box,
+  Button,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from '@mui/material';
+import { Scrollbar } from 'src/components/scrollbar';
+
+export const OrdersTable = (props) => {
+  const {
+    items = [],
+    handleClick,
+    open = false
+  } = props;
+
+  console.log("Items: ", items);
+
+  return (
+    <Card>
+      <Scrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  ID
+                </TableCell>
+                <TableCell>
+                  Status
+                </TableCell>
+                <TableCell>
+                  Ship
+                </TableCell>
+                <TableCell>
+                  Item
+                </TableCell>
+                <TableCell>
+                  Quantity
+                </TableCell>
+                <TableCell>
+                  Date
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((order) => {
+                return (
+                  <TableRow
+                    hover
+                    key={order.orderId}
+                  >
+                    <TableCell>
+                      {order.orderId}
+                    </TableCell>
+                    <TableCell>
+                      {order.status}
+                    </TableCell>
+                    <TableCell>
+                      {order.shipId != null ? order.shipId : "Not Assigned"}
+                    </TableCell>
+                    <TableCell>
+                      {order.item}
+                    </TableCell>
+                    <TableCell>
+                      {order.quantity}
+                    </TableCell>
+                    <TableCell>
+                      {order.date}
+                    </TableCell>
+                    <TableCell>
+                      <Button onClick={() => handleClick(order.orderId)}>View</Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+      </Scrollbar>
+    </Card>
+  );
+};
+
+OrdersTable.propTypes = {
+  count: PropTypes.number,
+  items: PropTypes.array,
+  handleClick: PropTypes.func,
+  open: PropTypes.bool
+};
